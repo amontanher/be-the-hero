012cf4b4183052e56dcaef274fa4d813304de396
alessandra <alessandraamontanher@gmail.com>
2020-03-25 13:44:04 -0300

    reat-router-dom & logon css js

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
+import "./styles.css";
+
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
+
+export default function Register() {
+  return <div></div>;
+}
